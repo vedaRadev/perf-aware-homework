280af90ca5a993c02711b10624e57f83a7a45e8f
vedaRadev <ryancarmstrong97@gmail.com>
2024-08-21 13:41:32 -0400

    add more tests to asm nop loops repetition test

--- repetition_tests/asm_nop_loops/src/main.rs
+++ repetition_tests/asm_nop_loops/src/main.rs
@@ -8,10 +8,13 @@ use repetition_tester::{
 extern "C" {
     fn nop_3x1_all_bytes(len: u64, buf: *mut u8);
     fn nop_1x3_all_bytes(len: u64, buf: *mut u8);
+    fn nop_3x3_all_bytes(len: u64, buf: *mut u8);
     fn nop_1x9_all_bytes(len: u64, buf: *mut u8);
+    fn nop_5x3_all_bytes(len: u64, buf: *mut u8);
+    fn nop_1x15_all_bytes(len: u64, buf: *mut u8);
 }
 
-const BUFFER_SIZE: usize = 2usize.pow(26); // 16mb
+const BUFFER_SIZE: usize = 2usize.pow(28); // 256mb
 struct Params { buffer: Vec<u8> }
 
 #[inline(never)]
@@ -34,6 +37,16 @@ fn test_nop_1x3_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
 
 #[inline(never)]
 #[no_mangle]
+fn test_nop_3x3_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
+    let len = buffer.len() as u64;
+    let test_section = TimeTestSection::begin();
+    unsafe { nop_3x3_all_bytes(len, buffer.as_mut_ptr()); }
+    test_section.end(len)
+}
+
+
+#[inline(never)]
+#[no_mangle]
 fn test_nop_1x9_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
     let len = buffer.len() as u64;
     let test_section = TimeTestSection::begin();
@@ -41,11 +54,34 @@ fn test_nop_1x9_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
     test_section.end(len)
 }
 
+#[inline(never)]
+#[no_mangle]
+fn test_nop_5x3_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
+    let len = buffer.len() as u64;
+    let test_section = TimeTestSection::begin();
+    unsafe { nop_5x3_all_bytes(len, buffer.as_mut_ptr()); }
+    test_section.end(len)
+}
+
+#[inline(never)]
+#[no_mangle]
+fn test_nop_1x15_all_bytes(Params { buffer }: &mut Params) -> TimeTestResult {
+    let len = buffer.len() as u64;
+    let test_section = TimeTestSection::begin();
+    unsafe { nop_1x15_all_bytes(len, buffer.as_mut_ptr()); }
+    test_section.end(len)
+}
+
 fn main() {
+    println!("buffer size: {BUFFER_SIZE}");
+
     let shared_params = Params { buffer: vec![0u8; BUFFER_SIZE] };
     let mut repetition_tester = RepetitionTester::new(shared_params);
     repetition_tester.register_test(test_nop_3x1_all_bytes, "nop 3x1 all bytes");
     repetition_tester.register_test(test_nop_1x3_all_bytes, "nop 1x3 all bytes");
+    repetition_tester.register_test(test_nop_3x3_all_bytes, "nop 3x3 all bytes");
     repetition_tester.register_test(test_nop_1x9_all_bytes, "nop 1x9 all bytes");
+    repetition_tester.register_test(test_nop_5x3_all_bytes, "nop 5x3 all bytes");
+    repetition_tester.register_test(test_nop_1x15_all_bytes, "nop 1x15 all bytes");
     repetition_tester.run_tests();
 }
--- repetition_tests/asm_nop_loops/src/nop_loops.asm
+++ repetition_tests/asm_nop_loops/src/nop_loops.asm
@@ -1,6 +1,9 @@
     global nop_3x1_all_bytes
     global nop_1x3_all_bytes
+    global nop_3x3_all_bytes
     global nop_1x9_all_bytes
+    global nop_5x3_all_bytes
+    global nop_1x15_all_bytes
 
     section .text
 
@@ -29,11 +32,64 @@ nop_1x3_all_bytes:
 .loop_end:
     ret
 
+nop_3x3_all_bytes:
+    xor rax, rax
+.loop_start:
+    cmp rcx, rax
+    jz .loop_end
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    inc rax
+    jmp .loop_start
+.loop_end:
+    ret
+
 nop_1x9_all_bytes:
     xor rax, rax
 .loop_start:
     cmp rcx, rax
     jz .loop_end
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    inc rax
+    jmp .loop_start
+.loop_end:
+    ret
+
+nop_5x3_all_bytes:
+    xor rax, rax
+.loop_start:
+    cmp rcx, rax
+    jz .loop_end
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    db 0x0f, 0x1f, 0x00 ; 3-byte NOP
+    inc rax
+    jmp .loop_start
+.loop_end:
+    ret
+
+nop_1x15_all_bytes:
+    xor rax, rax
+.loop_start:
+    cmp rcx, rax
+    jz .loop_end
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
     nop
     nop
     nop
